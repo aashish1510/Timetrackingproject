27bd599967558869c838dc61e05ddc3020cafb29
aashish1510 <ashu09081510@gmail.com>
2022-02-22 10:37:09 +0530

    team

---
+++ controller/project-team-controller.js
@@ -0,0 +1,55 @@
+const ProjectTeamModel = require("../model/project-team-model")
+const projectteamSchema=require("../model/project-team-model")
+
+
+module.exports.addTeam = function(req,res){
+    let user= req.body.user
+    let project = req.body.project
+
+
+    let team= new projectteamSchema({
+        user:user,
+        project:project,
+       
+
+    })
+    team.save(function(err,success){
+        if(err){
+            res.json({
+                msg:"something went wrong",
+                status:-1,
+                data:err
+            })
+            
+        }else{
+            res.json({
+                msg:"user added",
+                status:200,
+                data:success
+
+            })
+                
+            
+        }
+    })
+
+}    
+module.exports.getTeam = function(req,res){
+    ProjectTeamModel.find({project:req.query.projectid}).populate("user").populate("project").exec(function(err,data){
+        if(err){
+            res.json({
+                msg:"data not found",
+                status:-1,
+                data:err
+            })
+        }else{
+            res.json({
+                msg:"data retrived ",
+                status:200,
+                data:data
+            })
+        }
+
+    })
+}
+
